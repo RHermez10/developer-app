0658fc1275e3878f874c852a00c5e91eed24f4af
ramta hermez <ramta97@hotmail.com>
2026-05-13 15:47:46 +0200

    added toggle theme component

---
+++ components/shared/ThemeToggle.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { useTheme } from "next-themes";
+
+export default function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  return (
+    <button
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      className="border px-4 py-2 rounded-lg"
+    >
+      Toggle Theme
+    </button>
+  );
+}
