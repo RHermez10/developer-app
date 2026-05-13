03a216a341976f8d919cb85a88d39ad6e15fef6d
ramta hermez <ramta97@hotmail.com>
2026-05-13 21:01:38 +0200

    Created loginpage

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+export default  function LoginPage (){
+    return(
+        <div className="flex min-h-screen items-center justify-center bg-gray-950 text-white">
+            <div>
+                <h1 className="text-2xl font-bold">Login Page</h1>
+                <p className="mt-2 text-gray-400">Authentication coming later</p>
+            </div>
+        </div>
+    )
+}
