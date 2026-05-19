5237abc08090418a96c5c1849993b5faa8497311
ramta hermez <ramta97@hotmail.com>
2026-05-19 14:10:17 +0200

    created project schema

---
+++ features/projects/schema/project-schema.ts
@@ -0,0 +1,12 @@
+import {z} from "zod"
+export const projectSchema = z.object({
+    title: z.string().min(3, "Title must be at least 3 characters"),
+    description: z.string().min(10, "Description must be at least 10 characters"),
+    status: z.enum([
+        "Active",
+        "Completed",
+        "Archived"
+    ]),
+});
+
+export type ProjectFormData = z.infer< typeof projectSchema>
